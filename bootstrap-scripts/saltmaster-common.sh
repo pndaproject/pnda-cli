95007998a2deb2f210b2349b73f1676baf4caff5
sushant <sushant.pandey@maplelabs.com>
2017-12-30 13:24:08 +0000

    PNDA-3127: Post ingress aggregation for Kafka datasets.

--- bootstrap-scripts/saltmaster-common.sh
+++ bootstrap-scripts/saltmaster-common.sh
@@ -172,3 +172,17 @@ package_repository:
   fs_location_path: "$PR_FS_LOCATION_PATH"
 EOF
 fi
+
+if [ "$COMPACTION" == "YES" ] ; then
+cat << EOF >> /srv/salt/platform-salt/pillar/env_parameters.sls
+dataset_compaction:
+  compaction: $COMPACTION
+  pattern: '$PATTERN'
+EOF
+else
+cat << EOF >> /srv/salt/platform-salt/pillar/env_parameters.sls
+dataset_compaction:
+  compaction: NO
+  pattern: '$PATTERN'
+EOF
+fi
